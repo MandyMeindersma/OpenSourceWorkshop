6dec513731dfd7d7272a81bc45b3aebf7127a47b
Mandy Meindersma <mandy.m.meindersma@gmail.com>
2024-04-30 07:44:26 -0400

    Adding all student problems

---
+++ README.md
@@ -0,0 +1,3 @@
+# Open Source Workshop
+
+This repo contains a bunch of folders for students to make pull requests to to teach them about the open source workflow for issues, forking, reviewing, and merging in! 
---
+++ student_005/containsDuplicate.py
@@ -0,0 +1,24 @@
+def containsDuplicate(nums) -> bool:
+        for i in range(len(nums)):
+            for j in range(len(nums[i+1:len(nums)])):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+
+# Test Cases:
+
+### Example 1:
+print(containsDuplicate([1,2,3,1]))
+print("Expected: true")
+print("")
+
+### Example 2:
+print(containsDuplicate([1,2,3,4]))
+print("Expected: false")
+print("")
+
+### Example 3:
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+print("Expected: true")
+print("")
